621d42c7510a3923d0fd5604642e91ceeb5fa09c
larrysit0 <63930869+larrysit0@users.noreply.github.com>
2025-07-07 16:25:48 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -147,5 +147,4 @@ def twilio_voice():
 
 # ▶️ Ejecutar el servidor
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8000)
