b48f0cb25b84c2dacce9dd1c02328d5648f68e55
larrysit0 <63930869+larrysit0@users.noreply.github.com>
2025-07-12 21:35:46 -0500

    Update webapp_telegram.py

--- webapp_telegram.py
+++ webapp_telegram.py
@@ -1,22 +1,7 @@
 import requests
-import time
 
-TELEGRAM_BOT_TOKEN = "TU_TOKEN_AQUI"
+TOKEN = "TU_TOKEN_NUEVO_BOT"
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 
-def obtener_chat_ids():
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates"
-    response = requests.get(url)
-    data = response.json()
-
-    print("🔁 Resultados de Telegram:")
-    for update in data.get("result", []):
-        msg = update.get("message") or update.get("edited_message")
-        if msg:
-            chat = msg["chat"]
-            text = msg.get("text", "")
-            print(f"🆔 Chat ID: {chat['id']} | Nombre: {chat.get('title', 'privado')} | Texto: {text}")
-
-if __name__ == "__main__":
-    while True:
-        obtener_chat_ids()
-        time.sleep(5)
+response = requests.get(url)
+print(response.json())
